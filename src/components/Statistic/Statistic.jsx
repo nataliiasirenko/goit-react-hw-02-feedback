0ebe73c8675cf09177786aa73593bcd576e075a5
nataliiasirenko <sirenko.nataliia@hotmail.com>
2023-11-20 19:37:03 +0000

    percentage

--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -6,7 +6,7 @@ export const Statistic = ({ good, neutral, bad, total, positiveFeedback}) => {
         <li option = "neutral"> Neutral : {neutral}</li>
         <li option = "bad"> Bad : {bad}</li>
         <li>Total : {total}</li>
-        <li>Positive feedback : {positiveFeedback}</li>
+        <li>Positive feedback : {positiveFeedback} %</li>
         
     </ul>
 
